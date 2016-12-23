3a635dc66561caf29af5c531fd27ef2b7dc17ab7
Saqlain Gardezi <saqlaingardezi86@gmail.com>
2016-12-23 10:22:44 +0500

    Configured for saving sessions in production

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
-var express 	=	require('express');
-var app 		=	express();
-var path 		=	require('path');
+var express 		=	require('express');
+var app 			=	express();
+var path 			=	require('path');
+var cookieParser	= 	require('cookie-parser'),	 //For managing session
+	session 		=	require('express-session'),
+	config			=	require('./config/config.js'), //provides setting relevant to mode app is running
+		// In PRODUCTION sessions cant be stored in default memory store so,
+	ConnectMongo	=	require('connect-mongo')(session)	// store session in mongo lab account
+
+	;
 
 	// Setting hogan as templating engine
-app.set('views', path.join(__dirname,'views'));	// Set vies folder
+app.set('views', path.join(__dirname,'views'));	// Set views folder
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public'))); // look for Static files
+app.use(cookieParser()); // enable session
+
+var env		=	process.env.NODE_ENV || 'development';	
+if (env === 'development') {
+		// dev specific settings
+	app.use(session({secret:config.sessionSecret, saveUninitialized: true, resave: true}));
+}else{
+		// production specific settings
+	app.use(session({
+		secret: config.sessionSecret,
+		store: new ConnectMongo({ 	// stores session in mongolab
+			url: config.dbURL,
+			stingify: true
+			})
+		})
+	);
+}
 
 	//	Call routes.js module in app.js and invoke the function
 require('./routes/routes.js')(express,app);
 
-app.listen(3000, ()=> console.log("app running at port 3000"));
+app.listen(3000, ()=> {
+	console.log("app running at port 3000");
+	console.log("Mode : " + env);
+});
--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,22 @@
 module.exports	=	function(express, app){
 	var router	=	express.Router();
 
-	router.get('/', function(req, res){
-				//res.status(200).location('index.html');		<--Don't work
-				//res.status(status).send('index', {title: "Chatcat"});
+	router.get('/', function(req, res, next){
+
 		res.render('index', {title: "chat"});
 	});
-	router.get('/chatrooms', function(req, res){
+	router.get('/chatrooms', function(req, res, next){
 				//res.send('chatrooms', {title: 'Chatrooms'});   <--DON'T WORK
 		res.render('chatrooms', {title: 'Chatrooms'});
+	});
+
+	router.get('/setcolor', function(req,res,next){
+		req.session.favcolor="gray";
+		res.send("color set to " + req.session.favcolor);
+	});
+	router.get('/getcolor', function(req, res, next){
+		var color= req.session.favcolor !== undefined? req.session.favcolor : "not defined" ;
+		res.send("color " + color);
 	});
 
 		// set default route to instance of this router
